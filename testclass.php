5dc4ffcfb33c61b9f62516ee8bd3e74b930fb8b5
YuriPlusOne <yuri.plus.one@gmail.com>
2016-08-12 23:00:18 +0500

    Filled

--- testclass.php
+++ testclass.php
@@ -1,8 +1,18 @@
 <?php
 
 
+class Simply_class {
 
+	public $text = 'some text';
 
+	public function get_text() {
 
+		print $this->text;
 
-?>
+	}
+
+}
+
+$obj = new Simply_class();
+
+$obj->get_text();
